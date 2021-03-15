d2ddbe3b464c844752bc31119eb700b799b0375d
aaron_bangs <bangs.aaron@gmail.com>
2021-03-15 19:36:20 -0400

    Made it look even nicer, and changed the icon and favicon

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello!");
+
+//Change the favicon
+iconurl = chrome.runtime.getURL("icons/burger.png");
+favicons = document.querySelectorAll('[rel="SHORTCUT ICON"], [rel="apple-touch-icon"], [rel="shortcut icon"]');
+favicons.forEach(favicon => favicon.href = iconurl);
