7f7c2fded5f75c7e985c94805863f689bc6d3ca0
Tony Prebilic <TJJP@gmail.com>
2021-03-16 23:39:22 -0400

    Added a funtion to clear cart. Reloads page for each item removed. NEEDS TWEEKING Need to add a clear cart button as well.

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ for (i = 0; i < feature_boxes.length; i++) {
         radioButton 
     }
 } 
+
+// Clear cart funtion
+function clearCart() {
+    var cartProductRow = document.getElementsByClassName("cartProductRow")
+    var i;
+    for (i = 0; i < cartProductRow.length; i++) {
+        bread = cartProductRow[i] 
+        remove_button = bread.querySelectorAll("a")
+        window.location.href = remove_button[0];
+    }
+}
+
