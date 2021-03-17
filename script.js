42bffd93a58f78535c73a1ff6a71b988b686b542
Tony Prebilic <TJJP@gmail.com>
2021-03-16 22:41:29 -0400

    Attempted to add the change for radio buttons

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ console.log("Hello!");
 iconurl = chrome.runtime.getURL("icons/burger.png");
 favicons = document.querySelectorAll('[rel="SHORTCUT ICON"], [rel="apple-touch-icon"], [rel="shortcut icon"]');
 favicons.forEach(favicon => favicon.href = iconurl);
+
+//Change check boxes to radio buttons
+var feature_boxes = document.getElementsByClassName("featureset");
+bread = feature_boxes[0]
+max_span = bread.querySelectorAll("span")[1];
+var i;
+for (i = 0; i < feature_boxes.length; i++) {
+    if (max_span.innerText == "max: 1"){
+        radioButton = bread.querySelectorAll("input")
+        radioButton 
+    }
+} 
